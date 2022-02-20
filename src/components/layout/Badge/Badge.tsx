8ce977b2a517dd681f3b5210ca0f7b9cc7e1af94
Shubham Thorat <thoratshubham048@gmail.com>
2022-02-20 11:16:48 +0530

    like,dislike

--- src/components/layout/Badge/Badge.tsx
+++ src/components/layout/Badge/Badge.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import { Space, Popover } from "antd";
 import "./Badge.scss";
 
-const Badge = (props: { component: any; count?: number }) => {
-  const content = () => {
-    if (props.count) {
-      return <span> 10</span>;
-    } else return <></>;
-  };
+const Badge = (props: {
+  component: any;
+  count?: number;
+  increment?: Function;
+}) => {
   return (
-    <Space className="count_badge">
-      <Popover title={props.count ? props.count : null} content={null}>
-        {props.component}
-      </Popover>
+    <Space
+      className="count_badge"
+      onClick={() => (props.increment ? props.increment() : null)}
+    >
+      {props.component}
     </Space>
   );
 };
--- src/components/layout/Card/index.tsx
+++ src/components/layout/Card/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { List, Avatar, Space, Card } from "antd";
 import {
   DislikeOutlined,
@@ -10,15 +11,52 @@ import "./Card.scss";
 
 const CustomCard = (props: { href: string }) => {
   const { Meta } = Card;
+
+  const [like, setlike] = useState(0);
+  const [dislike, setdislike] = useState(0);
+  const [download, setdownload] = useState(0);
+  const likeIncrement = () => {
+    setlike(like + 1);
+  };
+  const dislikeIncrement = () => {
+    setdislike(dislike + 1);
+  };
+  const downloadIncrement = () => {
+    setdownload(download + 1);
+  };
   return (
     <Card
       style={{ width: 300, marginBottom: 50 }}
       className="card__container"
       cover={<img alt="example" src={props.href} />}
       actions={[
-        <Badge component={<LikeOutlined />} count={10} />,
-        <Badge component={<DislikeOutlined />} count={0} />,
-        <Badge component={<DownloadOutlined />} count={20} />,
+        <Badge
+          component={
+            <Space>
+              <LikeOutlined />
+              <span>{like}</span>
+            </Space>
+          }
+          increment={likeIncrement}
+        />,
+        <Badge
+          component={
+            <Space>
+              <DislikeOutlined />
+              <span>{dislike}</span>
+            </Space>
+          }
+          increment={dislikeIncrement}
+        />,
+        <Badge
+          component={
+            <Space>
+              <DownloadOutlined />
+              <span>{download}</span>
+            </Space>
+          }
+          increment={downloadIncrement}
+        />,
         <Badge component={<UploadOutlined />} />,
       ]}
     >
