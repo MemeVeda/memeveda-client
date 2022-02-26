acff3b6a56c89c5c372ea2c545831d4b83c14fbf
rayanubhab <rayanubhab@gmail.com>
2022-02-26 11:43:01 +0530

    Editing about page

--- src/components/pages/About/index.tsx
+++ src/components/pages/About/index.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { Content } from "antd/lib/layout/layout";
+import { Card, Col, Row } from 'antd';
 
 const About = () => {
-  return <Content className="site-layout-background">About us</Content>;
+  return (
+    <Content className="site-layout-background">
+     <div className="site-card-wrapper">
+    <Row gutter={16}>
+      <Col span={8}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+    </Row>
+  </div>,
+ 
+  </Content>
+  )
 };
 
 export default About;
