67a419db4281875e75a3b7c8a941971224848c90
Shubham Thorat <thoratshubham048@gmail.com>
2022-03-08 16:04:20 +0530

    convert data url to bas364

--- src/components/pages/MemeGenerator/index.tsx
+++ src/components/pages/MemeGenerator/index.tsx
@@ -1,4 +1,3 @@
-import { Input } from "antd";
 import "./MemeGenerator.scss";
 import "react-image-crop/src/ReactCrop.scss";
 import "tui-image-editor/dist/tui-image-editor.css";
@@ -7,62 +6,77 @@ import stempmv from "../../assets/StartingTemplate.png";
 import { saveAs } from "file-saver";
 import axios from "axios";
 import { BACKEND_URL } from "../../utils/contant";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import Notification from "../../layout/Notification";
 
 const MemeGenerator = () => {
+  const singleuser = useSelector((state: RootState) => state.user);
+
   //@ts-ignore
-  window.saveAs = (blob, imageName) => {
-    //@ts-ignore
-    let url = window.URL.createObjectURL(blob);
-    axios
-      .post(`${BACKEND_URL}/meme`, {
-        href: url,
-        tags: ["success"],
-      })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  window.saveAs = (blob: Blob, imageName: string) => {
+    if (
+      singleuser === undefined ||
+      singleuser.user_id === undefined ||
+      singleuser.user_id === ""
+    ) {
+      Notification({ message: "Authentication required" });
+      return;
+    }
+
+    let reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onload = async () => {
+      let base64 = reader.result;
+      await axios
+        .post(`${BACKEND_URL}/meme`, {
+          href: base64,
+          tags: ["success"],
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
     saveAs(blob, imageName);
   };
   return (
-    <>
-      {/* <img id="source_best" alt="" /> */}
-      <ImageEditor
-        includeUI={{
-          loadImage: {
-            path: stempmv,
-            name: "SampleImage",
-          },
-          //theme: myTheme,
-          menu: [
-            "crop",
-            "flip",
-            "rotate",
-            "draw",
-            "shape",
-            "icon",
-            "text",
-            "mask",
-            "filter",
-          ],
-          initMenu: "filter",
-          uiSize: {
-            width: "100%",
-            height: "91vh",
-          },
-          menuBarPosition: "bottom",
-        }}
-        cssMaxHeight={750}
-        cssMaxWidth={1000}
-        selectionStyle={{
-          cornerSize: 20,
-          rotatingPointOffset: 70,
-        }}
-        usageStatistics={false}
-      />
-    </>
+    <ImageEditor
+      includeUI={{
+        loadImage: {
+          path: stempmv,
+          name: "SampleImage",
+        },
+        //theme: myTheme,
+        menu: [
+          "crop",
+          "flip",
+          "rotate",
+          "draw",
+          "shape",
+          "icon",
+          "text",
+          "mask",
+          "filter",
+        ],
+        initMenu: "filter",
+        uiSize: {
+          width: "100%",
+          height: "91vh",
+        },
+        menuBarPosition: "bottom",
+      }}
+      cssMaxHeight={750}
+      cssMaxWidth={1000}
+      selectionStyle={{
+        cornerSize: 20,
+        rotatingPointOffset: 70,
+      }}
+      usageStatistics={false}
+    />
   );
 };
 
